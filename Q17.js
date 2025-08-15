24f8636b905a58d4257da8a94b20de8e78633b1e
Salma Sanubar <salmasanubar786@gmail.com>
2025-08-15 06:05:55 +0000

    Find Maximum without Math method

---
+++ Q17.js
@@ -0,0 +1,22 @@
+const myArray = [2,90,45,100,600]
+
+function findMaximum(arr){
+
+    if(arr.length===0){
+        return "Array is Empty"
+    }
+
+    let Maximum = arr[0]
+
+    for(let i = 1; i<arr.length; i++){
+
+        if(arr[i]>Maximum){
+            Maximum = arr[i]
+        }
+    }
+
+    return {Maximum}
+
+}
+
+console.log(`Maximum of ${myArray}:`, findMaximum(myArray))
