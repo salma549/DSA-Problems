7a167ea6dc9439238c385b07f681b07a793a2447
Salma Sanubar <117900157+salma549@users.noreply.github.com>
2025-09-08 13:49:39 +0530

    Update 26.js

--- 26.js
+++ 26.js
@@ -1,3 +1,5 @@
+// convert to Upper Case
+
 function isAlphabet(char){
     let upperChar = char.toUpperCase()
     
@@ -6,6 +8,29 @@ function isAlphabet(char){
     }else{
         return false;
     }
+}
+
+
+let char1 = 'j'
+let char2 = '3'
+
+console.log(`${char1} is alphabet? ${isAlphabet(char1)}`)
+console.log(`${char2} is alphabet? ${isAlphabet(char2)}`)
+
+
+// Another Method using CharCode
+
+function isAlphabet(char){
+    let charCode = char.charCodeAt(0)
+    
+    if((charCode>=65 && charCode<=90) || (charCode>=97 && charCode<=122)){
+        return true;
+    }else{
+        
+      return  false
+    }
+    
+   
 }
 
 
