7b69b7b6c681fd9bab61a424ea2721b2321b47ed
Salma Sanubar <117900157+salma549@users.noreply.github.com>
2025-09-08 12:23:06 +0530

    Program to find Leap Year

---
+++ 22.js
@@ -0,0 +1,14 @@
+function isLeap(year){
+    
+    if((year%4 === 0 && year%100 !==0) || year%400 === 0){
+        console.log(`${year} is a Leap Year`)
+    }else{
+                console.log(`${year} is a not Leap Year`)
+
+    }
+    
+}
+
+let year = 2024
+
+isLeap(year)
